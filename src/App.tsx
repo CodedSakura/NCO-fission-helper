e4a72a7176b06066d3f40c79eb2b5e75d9a3b307
thephisics101 <thephisics101@gmail.com>
2020-06-03 01:20:33 +0300

    rewrite radial menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import RadialMenu from "./Components/RadialMenu";
+import RadialMenu, {MenuChoice} from "./Components/RadialMenu";
 
 class App extends React.Component {
   render() {
-    let choices = [];
+    let choices1: MenuChoice[] = [];
     for (let i = 0; i < 10; i++) {
-      choices.push({name: `some text ${i}`, fn: () => console.log(i)})
+      choices1.push({name: `some text ${i}`, fn: () => console.log(i)})
     }
+    choices1[1].disabled = true;
     return <>
-      <RadialMenu choices={choices} active={1}/>
+      <RadialMenu choices={choices1}>
+        <div style={{width: 400, height: 400}}>asd</div>
+      </RadialMenu>
       Hello React!
     </>;
   }
--- src/Components/RadialMenu.tsx
+++ src/Components/RadialMenu.tsx
@@ -1,86 +1,138 @@
 import React, {Component} from 'react';
 
-import "../Style/RadialMenu.scss";
 import {classMap} from "../Utils/utils";
 
-interface Props {
-  choices: {name: string, fn: () => any}[]
-  active?: number
+import "../Style/RadialMenu.scss";
+
+export interface Config {
+  entryRadius: number
+  indicatorRadius: number
+  indicatorAngle: number // rad
+  shortTimeout: number // ms
 }
 
-interface State {
-  menuOpen: boolean
-  menuPosition: {x: number, y: number}
-  menuSelection: number
-  mouseOffset: [number, number]
+export const DefaultConfig: Config = {
+  entryRadius: 100,
+  indicatorRadius: 30,
+  indicatorAngle: Math.PI/2,
+  shortTimeout: 150,
 }
 
-const cfg = {
-  tr: 100,
-  ir: 30,
-  ia: Math.PI/2,
+enum MenuState {
+  Closed, Undecided, ClickOpen, HoldOpen,
 }
 
-class RadialMenu extends Component<Props, State> {
-  state: State = {
-    menuOpen: false,
-    menuPosition: {x: -1, y: -1},
-    menuSelection: -1,
-    mouseOffset: [0, 0],
+export interface MenuChoice {
+  name: string
+  fn: (e?: MouseEvent) => any
+  disabled?: boolean
+}
+
+interface RadialMenuProps {
+  choices: MenuChoice[]
+  config?: Partial<Config>
+  children: JSX.Element
+}
+
+interface RadialMenuState {
+  menuState: MenuState
+  menuPosition: [number, number]
+  mouseAngle: number|null
+  mouseDist2: number
+}
+
+class RadialMenu extends Component<RadialMenuProps, RadialMenuState> {
+  state: RadialMenuState = {
+    menuState: MenuState.Closed,
+    menuPosition: [0, 0],
+    mouseAngle: null,
+    mouseDist2: -1
   }
+  mouse2down = false;
+  originalEvt: MouseEvent|undefined;
+  wrapper: HTMLDivElement|null = null;
 
-  componentDidMount() {
-    document.addEventListener("contextmenu", this.menuListener);
-  }
   componentWillUnmount() {
-    document.removeEventListener("contextmenu", this.menuListener);
+    if (this.props.children instanceof Element)
+      this.props.children.removeEventListener("contextmenu", this.menuListener);
+  }
+
+  menuListener = (e: Event) => ((e: MouseEvent) => {
+    e.preventDefault();
+    this.originalEvt = e;
+    this.mouse2down = true;
+    this.setState({menuState: MenuState.Undecided, menuPosition: [e.pageX, e.pageY]});
+    document.addEventListener("mousemove", this.mouseMoveListener);
+    document.addEventListener("mouseup", this.mouseUpListener);
+    this.mouseMoveListener(e);
+    setTimeout(() => {
+      this.setState({menuState: this.mouse2down ? MenuState.HoldOpen : MenuState.ClickOpen});
+    }, {...DefaultConfig, ...this.props.config}.shortTimeout);
+  })(e as MouseEvent);
+
+  mouseMoveListener = (e: MouseEvent) => {
+    const [x, y] = this.state.menuPosition;
+    const rx = e.x - x, ry = e.y - y;
+    const dist2 = rx**2 + ry**2
+    const outside = dist2 > {...DefaultConfig, ...this.props.config}.indicatorRadius ** 2;
+    const angle = outside ?
+      (Math.atan2(ry, rx) + 5*Math.PI/2) % (Math.PI*2) :
+      null;
+    this.setState({mouseAngle: angle, mouseDist2: dist2});
   }
 
   mouseUpListener = (e: MouseEvent) => {
-    if (e.button === 2 && this.state.menuSelection >= 0 && this.state.menuSelection !== this.props.active)
-      this.props.choices[this.state.menuSelection].fn();
-    this.setState({menuOpen: false, menuSelection: -1, mouseOffset: [0, 0]});
-    document.removeEventListener("mousemove", this.mouseMoveListener);
-    document.removeEventListener("mouseup", this.mouseUpListener);
-  }
-
-  menuListener = (e: MouseEvent) => {
-    e.preventDefault();
-    this.setState({menuOpen: true, menuPosition: {x: e.x, y: e.y}});
-    document.addEventListener("mousemove", this.mouseMoveListener);
-    document.addEventListener("mouseup", this.mouseUpListener);
-  }
-
-  mouseMoveListener = (e: MouseEvent) => {
-    const {y, x} = this.state.menuPosition;
-    const rx = e.x - x, ry = e.y - y;
-    const {length} = this.props.choices;
-    const outside = rx**2 + ry**2 > cfg.ir**2;
-    const newV = outside ?
-      ((Math.atan2(ry, rx) + 5*Math.PI/2 + Math.PI/length) % (Math.PI*2)) / (2*Math.PI) * length << 0 :
-      -1;
-    this.setState({menuSelection: newV, mouseOffset: outside ? [rx, ry] : [0, 0]});
+    if (e.button === 2) {
+      this.mouse2down = false;
+    }
+    switch (this.state.menuState) {
+      case MenuState.Undecided:
+        return;
+      case MenuState.ClickOpen:
+        // TODO
+        break;
+      case MenuState.HoldOpen:
+        if (e.button === 2 && this.state.mouseAngle !== null) {
+          let choice = this.props.choices[this.state.mouseAngle / this.props.choices.length << 0];
+          if (!choice.disabled) choice.fn();
+        }
+        this.setState({menuState: MenuState.Closed, mouseAngle: null});
+        document.removeEventListener("mousemove", this.mouseMoveListener);
+        document.removeEventListener("mouseup", this.mouseUpListener);
+        break;
+    }
   }
 
   render() {
-    const {choices, active} = this.props;
-    const {menuOpen, menuPosition, menuSelection, mouseOffset} = this.state;
-    if (!menuOpen) return null;
-    return <div className="__menu_radial" style={{top: menuPosition.y, left: menuPosition.x}}>
-      <svg viewBox={`${-cfg.ir-5} ${-cfg.ir-5} ${cfg.ir*2+10} ${cfg.ir*2+10}`} width={cfg.ir*2}
-           style={{transform: "translate(-50%, -50%)"}} className="__menu_radial-indicator">
-        <g fill="none" transform={`rotate(${(Math.atan2(mouseOffset[1], mouseOffset[0]) - cfg.ia/2)/Math.PI*180})`}>
-          <path d={`M${cfg.ir} 0A${cfg.ir} ${cfg.ir} 0 1 0 ${-cfg.ir} 0A${cfg.ir} ${cfg.ir} 0 1 0 ${cfg.ir} 0`} className="__menu_radial-indicator-bg"/>
-          {mouseOffset.every(v => v === 0) ? undefined :
-            <path d={`M${cfg.ir} 0A${cfg.ir} ${cfg.ir} 0 0 1 ${Math.cos(cfg.ia) * cfg.ir} ${Math.sin(cfg.ia) * cfg.ir}`} className="__menu_radial-indicator-fg"/>
-          }
-        </g>
-      </svg>
-      {choices.map((v, i, {length}) => {
-        const ox = Math.sin(2*Math.PI/length*i)*cfg.tr, oy = Math.cos(2*Math.PI/length*i)*cfg.tr;
-        return <span key={i} style={{top: `${-oy}px`, left: `${ox}px`}}
-          className={classMap("__menu_radial-select", menuSelection === i && "__menu_radial-selection", active === i && "__menu_radial-active")}>{v.name}</span>
-      })}
+    const {choices} = this.props;
+    const {menuPosition, menuState, mouseAngle} = this.state;
+    const {indicatorRadius: ir, entryRadius: tr, indicatorAngle: ia} = {...DefaultConfig, ...this.props.config};
+    return <div ref={ref => {
+      this.wrapper = ref;
+      if (ref) ref.addEventListener("contextmenu", this.menuListener);
+    }}>
+      {this.props.children}
+      {menuState === MenuState.Closed ? null :
+        <div className="__menu_radial" style={{top: menuPosition[1], left: menuPosition[0]}}>
+          <svg viewBox={`${-ir - 5} ${-ir - 5} ${ir * 2 + 10} ${ir * 2 + 10}`} width={ir * 2}
+               style={{transform: "translate(-50%, -50%)"}} className="__menu_radial-indicator">
+            <g fill="none">
+              <path d={`M${ir} 0A${ir} ${ir} 0 1 0 ${-ir} 0A${ir} ${ir} 0 1 0 ${ir} 0`} className="__menu_radial-indicator-bg"/>
+              {mouseAngle !== null ?
+                <path transform={`rotate(${(mouseAngle - ia/2 - Math.PI/2) / Math.PI * 180})`} className="__menu_radial-indicator-fg"
+                      d={`M${ir} 0A${ir} ${ir} 0 0 1 ${Math.cos(ia) * ir} ${Math.sin(ia) * ir}`}/> :
+                undefined
+              }
+            </g>
+          </svg>
+          {choices.map((v, i, {length}) => {
+            const ox = Math.sin(2 * Math.PI / length * i) * tr, oy = Math.cos(2 * Math.PI / length * i) * tr;
+            const sel = mouseAngle === null ? -1 : (mouseAngle + Math.PI/length) * length / (2 * Math.PI) << 0;
+            return <span key={i} style={{top: `${-oy}px`, left: `${ox}px`}}
+                         className={classMap("__menu_radial-select", sel === i && "__menu_radial-selection", v.disabled && "__menu_radial-active")}>{v.name}</span>
+          })}
+        </div>
+      }
     </div>
   }
 }
